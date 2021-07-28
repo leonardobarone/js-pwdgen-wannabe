402da78c3762376645026cea69c486fa3046a5a5
leonardobarone <leonardobarone26@gmail.com>
2021-07-28 15:35:56 +0200

    esercizio fatto!

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+// Chiedi all'utente il suo nome
 var nome = prompt("Qual è il tuo nome?");
-
-document.getElementById('nome').innerHTML = nome;
+// Chiedi all'utente il suo cognome
+var cognome = prompt('Qual è il tuo cognome?');
+// Chiedi all'utente il suo colore preferito
+var colorePreferito = prompt('Qual è il tuo colore preferito?');
+// Scrivi sulla pagine nomecognomecolorepreferito40
+document.getElementById('risultato').innerHTML = nome + cognome + colorePreferito + 40;
